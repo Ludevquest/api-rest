7f54802e5db84d0cf601f722a1641d5ab72b826c
Ludevquest <ludevprogbr19@gmail.com>
2024-08-23 15:29:20 -0300

    feat: add implementation of the instructor list

---
+++ src/controllers/instrutores.ts
@@ -0,0 +1,24 @@
+import {Request, Response} from 'express'
+
+type TInstrutores = {
+  id: number
+  nome: string
+  email: string
+}
+
+const instrutores: TInstrutores[] = [
+  {
+    id: 1,
+    nome: 'Luiz',
+    email: 'luiz@email.com'
+  },
+  {
+    id: 2,
+    nome: 'Maria',
+    email: 'maria@email.com'
+  }
+]
+
+export const listar = (req: Request, res: Response) => {
+  return res.status(200).json(instrutores)
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express'
-import { teste } from './controllers/controller'
+import { listar } from './controllers/instrutores'
 
 const routes = Router()
 
-routes.get('/', teste)
+// listar todos instrutores
+routes.get('/instrutores', listar)
+
+// detalhar as informações de um instrutor
+// cadastrar um instrutor
+// editar um instrutor
+// excluir um instrutor
+
+// cadastrar uma aula para um instrutor
+//excluir uma aula para um instrutor
 
 export default routes
